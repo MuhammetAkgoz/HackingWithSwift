4a8eb6dbb4f8594d4e23e5e1eb55ce412c32caa4
MuhammetAkgoz <muhammet.akgz@gmail.com>
2023-12-06 20:18:14 +0300

    Initial Commit

---
+++ HackingSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackingSwift
+//
+//  Created by Muhammet Akgöz on 6.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackingSwift/HackingSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingSwiftApp.swift
+//  HackingSwift
+//
+//  Created by Muhammet Akgöz on 6.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HackingSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
