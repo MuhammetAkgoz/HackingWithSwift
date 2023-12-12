64338136d8546992bf467f518e659abac576ac2c
MuhammetAkgoz <muhammet.akgz@gmail.com>
2023-12-12 20:44:57 +0300

    Text should have exactly number of lines of height

--- HackingSwift/WorkingStaticText/EnteryView.swift
+++ HackingSwift/WorkingStaticText/EnteryView.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 struct EnteryView: View {
     var body: some View {
-        Text("This is some longer text that is limited to three lines maximum, so anything more than that will cause the text to clip.")
-            .lineLimit(3)
-            .frame(width:200)
+        Text("This is always two lines")
+            .lineLimit(3,reservesSpace: true)
+            .background(.red)
     }
 }
 
