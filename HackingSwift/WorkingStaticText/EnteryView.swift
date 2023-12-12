adaa34242c7971f547aa695ed8eb2ce394c87bde
MuhammetAkgoz <muhammet.akgz@gmail.com>
2023-12-12 20:46:09 +0300

    Truncation Mode

--- HackingSwift/WorkingStaticText/EnteryView.swift
+++ HackingSwift/WorkingStaticText/EnteryView.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 struct EnteryView: View {
     var body: some View {
-        Text("This is always two lines")
-            .lineLimit(3,reservesSpace: true)
-            .background(.red)
+        Text("This is an extremely long string of text that will never fit even the widest of iOS devices even if the user has their Dynamic Type setting as small as is possible, so in theory it should definitely demonstrate truncationMode().")
+            .lineLimit(1)
+            .truncationMode(.middle)
+            .frame(width: 200)
     }
 }
 
