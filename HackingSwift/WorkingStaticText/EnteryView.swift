fd911dbed772c8e9fd40a567fbb0eb30c7a1978c
MuhammetAkgoz <muhammet.akgz@gmail.com>
2023-12-12 20:52:06 +0300

    Font

--- HackingSwift/WorkingStaticText/EnteryView.swift
+++ HackingSwift/WorkingStaticText/EnteryView.swift
@@ -9,10 +9,16 @@ import SwiftUI
 
 struct EnteryView: View {
     var body: some View {
-        Text("This is an extremely long string of text that will never fit even the widest of iOS devices even if the user has their Dynamic Type setting as small as is possible, so in theory it should definitely demonstrate truncationMode().")
-            .lineLimit(1)
-            .truncationMode(.middle)
-            .frame(width: 200)
+        Text("This is an extremely long text string that will never fit even the widest of phones without wrapping")
+            .padding()
+            .background(.red)
+            .foregroundStyle(.white)
+            .fontDesign(.serif)
+            .fontWidth(.expanded)
+            .lineSpacing(20)
+            .frame(width:200)
+        
+        
     }
 }
 
